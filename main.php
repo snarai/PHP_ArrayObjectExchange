5a581afd03cf2c811d7474ee2d71cdba706cc3ce
snarai <funkastic.jazz@gmail.com>
2022-04-16 10:58:05 +0900

    Create main.php

---
+++ main.php
@@ -0,0 +1,27 @@
+class Member{
+    //インスタンス変数
+    public int $id;
+    public string $name;
+    public int $age;
+
+    //コンストラクタ
+    public function __construct(int $id, string $name, $age){
+        $this->id = $id;
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    //メソッド
+    public function getInfo(){
+        return $this->id ." ". $this->name. " ". $this->age;
+    }
+}
+
+$user1 = new Member(0006, "織田信長", 49);
+echo $user1->id. "\n";
+echo $user1->name. "\n";
+echo $user1->age. "\n";
+echo $user1->getInfo(). "\n";
+
+$user1->age = 50;
+echo $user1->getInfo(). "\n";
